191ff95ed910884bff03d7b3939d2d16117184e0
Digamber Singh <admin@remotestack.io>
2020-09-17 21:13:50 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+
+import { NgAuthService } from "./ng-auth.service";
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebae),
+    AngularFireAuthModule,
+    AngularFirestoreModule,    
+  ],
+  providers: [NgAuthService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
